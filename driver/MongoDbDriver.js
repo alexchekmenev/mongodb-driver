ed80e511fdd6f187bfd036e1703227bc9a550d51
Alexander Chekmenev <alexchekmenev@mail.ru>
2020-08-19 04:50:32 +0300

    feat: add MongoDbInit

--- driver/MongoDbDriver.js
+++ driver/MongoDbDriver.js
@@ -1,5 +1,6 @@
 const QueryParser = require('../src/QueryParser')
 const QueryRewriter = require('../src/QueryRewriter')
+const MongoClient = require('mongodb').MongoClient;
 
 const input = 'SELECT count(propA) a, Count(*) total, `donors` `donors__donor_state`, lol `donors__count` ' +
     'FROM b, test.donors AS `donors`, a WHERE a = "blabla" OR 1 > 0 GROUP BY 1 ASC, state, COUNT(a) ' +
@@ -10,10 +11,46 @@ const input1 = 'SELECT\n' +
     '  test.donors AS `donors` WHERE\n' +
     '  10 < 100 && `donors`."Donor City" = "San Francisco" LIMIT 10000'
 
-const parser = new QueryParser()
-const parsedSqlQuery = parser.parse(input)
-console.log('SQL query', JSON.stringify(parsedSqlQuery, null, 2))
 
-const rewriter = new QueryRewriter()
-const mongoDbPipeline = rewriter.rewrite(parsedSqlQuery)
-console.log('MongoDB', JSON.stringify(mongoDbPipeline, null, 2))
+const dbName = 'cube-js';
+const user = encodeURIComponent('root');
+const password = encodeURIComponent('rootpassword');
+const authMechanism = 'DEFAULT';
+const url = `mongodb://${user}:${password}@localhost:27017/?authMechanism=${authMechanism}`;
+
+async function connect() {
+    return new Promise((resolve, reject) => {
+        const client = new MongoClient(url);
+        client.connect(function(err) {
+            if (err) {
+                return reject(err)
+            }
+            console.log("Connected successfully to server");
+            resolve(client)
+        });
+    })
+}
+
+async function main(input) {
+    const client = await connect();
+
+    const parser = new QueryParser()
+    const parsedSqlQuery = parser.parse(input)
+    console.log('SQL query', JSON.stringify(parsedSqlQuery, null, 2))
+
+    const rewriter = new QueryRewriter()
+    const mongoDbPipeline = rewriter.rewrite(parsedSqlQuery)
+    console.log('MongoDB', JSON.stringify(mongoDbPipeline, null, 2))
+
+    try {
+        const db = client.db(dbName)
+        const q = await db.collection(mongoDbPipeline.collectionName).aggregate(mongoDbPipeline.pipeline).toArray()
+        console.log(q)
+    } catch (e) {
+        console.error(e)
+    } finally {
+        client.close()
+    }
+}
+
+main('SELECT Count(*) as total FROM donors where 1')
---
+++ src/MongoDbInit.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const parse = require('csv-parse');
+const MongoClient = require('mongodb').MongoClient;
+
+const dbName = 'cube-js ';
+const user = encodeURIComponent('root');
+const password = encodeURIComponent('rootpassword');
+const authMechanism = 'DEFAULT';
+const url = `mongodb://${user}:${password}@localhost:27017/?authMechanism=${authMechanism}`;
+
+const csvData=[];
+fs.createReadStream('./Donors.csv')
+    .pipe(parse({delimiter: ','}))
+    .on('data', function(csvrow) {
+        csvData.push(csvrow);
+    })
+    .on('end',async function() {
+        const client = await connect()
+        await insertRows(client, csvData)
+        client.close()
+    });
+
+async function connect() {
+    return new Promise((resolve, reject) => {
+        const client = new MongoClient(url);
+        client.connect(function(err) {
+            if (err) {
+                return reject(err)
+            }
+            console.log("Connected successfully to server");
+            resolve(client)
+        });
+    })
+}
+
+async function insertRows(client, data) {
+    const db = client.db(dbName)
+    const collection = db.collection('donors')
+    for (let i = 1; i < data.length; i += 100_000) {
+        const docs = data.slice(i, Math.min(i + 100_000, data.length)).map(row => {
+            return data[0].reduce((acc, field, index) => {
+                return {...acc, [field]: row[index]}
+            }, {})
+        })
+        await collection.insertMany(docs)
+    }
+}
